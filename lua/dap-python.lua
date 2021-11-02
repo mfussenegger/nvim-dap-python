b9e7672937c53a121ee5e4e8852b8e70985b56dd
Alexander Bessonov <nonsleepr@gmail.com>
2021-11-02 09:08:35 -0400

    Add a separate option to launch with arguments

--- lua/dap-python.lua
+++ lua/dap-python.lua
@@ -77,13 +77,16 @@ function M.setup(adapter_python_path, opts)
       request = 'launch';
       name = 'Launch file';
       program = '${workspaceFolder}/${file}';
+      console = opts.console;
+    })
+    table.insert(dap.configurations.python, {
+      type = 'python';
+      request = 'launch';
+      name = 'Launch file with arguments';
+      program = '${workspaceFolder}/${file}';
       args = function()
         local args_string = vim.fn.input('Arguments: ')
-        local a = {}
-        for arg in string.gmatch(args_string, "%S+") do
-          table.insert(a, arg)
-        end
-        return a
+        return vim.split(args_string, " +")
       end;
       console = opts.console;
     })
