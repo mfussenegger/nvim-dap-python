edfdd81242d550f199fc5d15727786a3c052410e
Mathias Fussenegger <f.mathias@zignar.net>
2021-11-20 19:28:43 +0100

    Adjust program path to use ${file} only
    See https://github.com/mfussenegger/nvim-dap/pull/353

--- lua/dap-python.lua
+++ lua/dap-python.lua
@@ -76,14 +76,14 @@ function M.setup(adapter_python_path, opts)
       type = 'python';
       request = 'launch';
       name = 'Launch file';
-      program = '${workspaceFolder}/${file}';
+      program = '${file}';
       console = opts.console;
     })
     table.insert(dap.configurations.python, {
       type = 'python';
       request = 'launch';
       name = 'Launch file with arguments';
-      program = '${workspaceFolder}/${file}';
+      program = '${file}';
       args = function()
         local args_string = vim.fn.input('Arguments: ')
         return vim.split(args_string, " +")
