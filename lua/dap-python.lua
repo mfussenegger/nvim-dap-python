c3c28f64a3366121211a3bdc7ce73ca5ee4007c4
Mathias Fussenegger <f.mathias@zignar.net>
2024-11-29 20:39:28 +0100

    Use dap.utils splitstr if available for file:args config
    It treats quoted strings as one argument

--- lua/dap-python.lua
+++ lua/dap-python.lua
@@ -265,6 +265,10 @@ function M.setup(python_path, opts)
       program = '${file}';
       args = function()
         local args_string = vim.fn.input('Arguments: ')
+        local utils = require("dap.utils")
+        if utils.splitstr and vim.fn.has("nvim-0.10") == 1 then
+          return utils.splitstr(args_string)
+        end
         return vim.split(args_string, " +")
       end;
       console = opts.console;
