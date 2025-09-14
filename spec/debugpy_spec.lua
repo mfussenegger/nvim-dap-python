15be55a04f90ed50a989b879fe73c4b0c45d01ea
Mathias Fussenegger <f.mathias@zignar.net>
2025-09-14 07:09:13 +0200

    Fix 'env' array serialization error on empty envfile

--- spec/debugpy_spec.lua
+++ spec/debugpy_spec.lua
@@ -53,6 +53,31 @@ describe('dap with debugpy', function()
     }
     assert.are.same(expected, dap.session().config)
   end)
+
+  it("Can start session with empty envfile", function()
+    local program = vim.fn.expand("%:p:h") .. "/spec/simple.py"
+    local config = {
+      type = "debugpy",
+      request = "launch",
+      name = "Launch file",
+      subProcess = false,
+      program = program,
+      envFile = "${workspaceFolder}/spec/.env-empty",
+    }
+    local win = api.nvim_get_current_win()
+    local bufnr = vim.fn.bufadd(program)
+    api.nvim_win_set_buf(win, bufnr)
+    api.nvim_win_set_cursor(win, { 8, 0 })
+    dap.toggle_breakpoint()
+    dap.run(config)
+    vim.wait(1000, function()
+      local session = dap.session()
+      return (session and session.initialized or false)
+    end)
+    local session = dap.session()
+    assert(session)
+    assert.are.same(vim.empty_dict(), session.config.env)
+  end)
 end)
 
 vim.fn.delete(venv_dir, 'rf')
